b6828cc70a9697067a74258d002feb9c05b12825
ianand3977 <anandgu2002@gmail.com>
2024-09-09 21:30:44 +0530

    postgress deployment configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,27 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres',
+// });
+
+
+// sequelize.authenticate()
+//   .then(() => console.log('Database connected'))
+//   .catch(err => console.log('Error: ' + err));
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,  // Important for Heroku/Render SSL connections
+      rejectUnauthorized: false,  // Important for SSL/TLS connections
     }
   }
 });
@@ -17,3 +31,5 @@ sequelize.authenticate()
   .catch(err => console.log('Error: ' + err));
 
 module.exports = sequelize;
+
+
