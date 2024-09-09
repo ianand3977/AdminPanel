c277b829edb006ceacb0fd79f90b6a10397a526a
ianand3977 <anandgu2002@gmail.com>
2024-09-09 20:30:21 +0530

    postgres connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+require('dotenv').config(); // Ensure this is at the top to load environment variables
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+// Create Sequelize instance using DATABASE_URL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // Disable logging if not needed
 });
 
 sequelize.authenticate()
